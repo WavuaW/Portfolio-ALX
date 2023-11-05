b82ee94cada99daf02594b33c0a0e3b3aa45562c
WavuaW <wavuamuroka@gmail.com>
2023-11-05 21:39:51 +0300

    Photo CRUD Operations

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages       
-from .forms import SignUpForm, AddRecordForm
+from .forms import SignUpForm, AddRecordForm, PhotoForm
 from .models import Record
 
 # Create your views here.
